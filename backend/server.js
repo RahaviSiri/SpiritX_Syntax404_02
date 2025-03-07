fde48e6f9b59511f9658f263992aa8de71f118f9
RahaviSiri <rahavi24siri@gmail.com>
2025-03-07 23:17:20 +0530

    Add backend and frontend files

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors"
+import dotenv from "dotenv";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 4500;
+
+connectDB();
+connectCloudinary();
+
+app.get("/", (req,res) => {
+    res.json("API working");
+});
+
+app.listen(PORT,() => {
+    console.log(`Backend is working at port ${PORT}`);
+})
