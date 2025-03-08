25f14cb6f576c55e23cdadc014fd34a606aeff0b
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-03-08 12:56:26 +0530

    category page

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,11 @@
+import batsman from './batsman.png'
+import bowler from './bowler.png'
+import all_rounder from './all_rounder.png'
+
+const assets = {
+    batsman,
+    bowler,
+    all_rounder,
+}
+
+export default assets;
