c4f39414fe93a329ebdff62d49403e6a0424dff6
Shaamma <shaamasajahan@gmail.com>
2025-03-08 21:29:37 +0530

    Add Home logic

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,11 +1,16 @@
 import batsman from './batsman.jpg'
 import bowler from './bowler.jpg'
 import all_rounder from './all_rounder.jpg'
+import background from './bg.jpg'
+import icon from './icon..jpg'
+import cricketer from './cricketer.jpg'
 
-const assets = {
+export const assets = {
     batsman,
     bowler,
     all_rounder,
+    background,
+    icon,
+    cricketer
 }
 
-export default assets;
